5908e318488d0fc0e2f7fd3d18aefe14dc50f582
priyanka437 <priyanka.r@adappt.co>
2020-03-31 16:40:47 +0530

    Added button for addBudject page

--- budject/src/screens/AddBudject.js
+++ budject/src/screens/AddBudject.js
@@ -1,30 +1,52 @@
 import React from 'react';
-import { View, StyleSheet,Image, Text } from 'react-native';
+import { View, StyleSheet, Image, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import LinearGradient from 'react-native-linear-gradient';
 
 const TabIcon = (props) => (
   <Icon name="md-add" color={props.focused ? 'red' : '#000000'} size={25} />
-) 
+)
 export default class AddBudject extends React.Component {
 
   static navigationOptions = {
     tabBarIcon: TabIcon
   };
-  
+
   render() {
     return (
       <View style={styles.container} >
-       <Image source={require('../../assets/add_bud_img.jpg')} style={styles.budImg} />
+        <Image source={require('../../assets/add_bud_img.jpg')} style={styles.budImg} />
+        <LinearGradient colors={['#ff66cc', '#ffccff', '#66ccff']} >
+        <LinearGradient colors={['#003366', '#0066ff', '#003366']} style={styles.linearGradient}>
+          <Text style={styles.buttonText}>
+            Add Budject
+  </Text>
+        </LinearGradient>
+        </LinearGradient>
       </View>
     );
   }
 }
 const styles = StyleSheet.create({
   container: {
-  flex: 1
+    flex: 1
   },
-  budImg:{
+  budImg: {
     width: 500,
     height: 400
-  }
+  },
+  linearGradient: {
+    borderRadius: 5,
+    margin: 85,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    fontSize: 18,
+    fontFamily: 'Gill Sans',
+    textAlign: 'center',
+    margin: 10,
+    color: '#ffffff',
+    backgroundColor: 'transparent',
+  },
 });
